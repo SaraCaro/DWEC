a8ac6f405f1a8392fc1d8a58ce88543489dae8c6
SaraCaro <saracarmyn@gmail.com>
2022-11-11 19:03:32 +0100

    Subida Ejercicios Temas4-5

---
+++ Tema 4/Ejercicios/Worksheet1/Ejercicio10/index.js
@@ -0,0 +1,44 @@
+function startDrag(e) {
+    if (e.target) {
+      targ = e.target;
+    } else {
+      targ = e.srcElement;
+    }
+  
+    offsetX = e.clientX;
+    offsetY = e.clientY;
+  
+
+    if (!targ.style.left) {
+      targ.style.left = "0px";
+    }
+    if (!targ.style.top) {
+      targ.style.top = "0px";
+    }
+  
+    coordX = parseInt(targ.style.left);
+    coordY = parseInt(targ.style.top);
+    drag = true;
+  
+    document.onmousemove = dragImg;
+    return false;
+  }
+  
+  function dragImg(e) {
+    if (!drag) {
+      return false;
+    }
+  
+    targ.style.left = coordX + e.clientX - offsetX + "px";
+    targ.style.top = coordY + e.clientY - offsetY + "px";
+    return false;
+  }
+  
+  function stopDrag() {
+    drag = false;
+  }
+  
+  window.onload = () => {
+    document.onmousedown = startDrag;
+    document.onmouseup = stopDrag;
+  };
---
+++ Tema 5/Ejercicios/Worksheet3/Ejercicio4/index.js
@@ -0,0 +1,52 @@
+let form = document.getElementById('form');
+let input = document.getElementById('addItems');
+let list = document.getElementById('groceryList');
+let submit = document.getElementById('submit');
+let clearItems = document.getElementById('clear');
+let deleteButton = document.getElementsByClassName('delete');
+let changeItem = document.getElementsByClassName('change');
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let value = input.value;
+    let li = document.createElement('li');
+    let span = document.createElement('span');
+    span.innerHTML = value;
+    list.appendChild(li);
+    li.appendChild(span);
+    input.value = '';
+
+    let deleteButton = document.createElement('button');
+    let changeItem = document.createElement('button');
+    deleteButton.innerHTML = 'X';
+    changeItem.innerHTML = 'change';
+    li.appendChild(deleteButton);
+    li.appendChild(changeItem);
+
+    deleteButton.className = 'delete';
+    changeItem.className = 'change';
+
+    for(let i = 0; i < deleteButton.length; i++) {
+        deleteButton[i].addEventListener('click', (event) => {
+            let li = event.target.parentElement;
+            li.parentElement.removeChild(li);
+        });
+    }
+
+    for(let i = 0; i < changeItem.length; i++) {
+        changeItem[i].addEventListener('click', (event) => {
+            let userValue = prompt('Enter new item');
+            let li = event.target.parentElement;
+            let span = li.firstElementChild;
+            span.textContent = userValue;
+        });
+    }
+});
+
+
+clearItems.addEventListener('click', () => {
+    while(list.firstChild) {
+        list.removeChild(list.lastChild);
+    }
+});
